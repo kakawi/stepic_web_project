aa8fbaeb1a5eea0fef7c545a279fec594af5e335
kakawi <glebon999@yandex.ru>
2016-03-10 21:16:40 +0300

    changes db

--- create_db.sh
+++ create_db.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 sudo service mysql start
 mysql -uroot -e "CREATE USER 'mysql_user'@'localhost' IDENTIFIED BY 'Qa4kT2e4XC'"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON * . * TO 'mysql_user'@'localhost'"
+mysql -uroot -e "FLUSH PRIVILEGES;"
 mysql -umysql_user -pQa4kT2e4XC -e "create database django_ask"
-mysql -umysql_user -pQa4kT2e4XC -e "GRANT ALL PRIVILEGES ON * . * TO 'mysql_user'@'localhost'"
-mysql -umysql_user -pQa4kT2e4XC -e "FLUSH PRIVILEGES;"
+
 
 python manage.py syncdb
