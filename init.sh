77aeb23683846175578687eca4c7c76ae170c1aa
kakawi <glebon999@yandex.ru>
2016-03-10 21:13:34 +0300

    c

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /bin/ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/gunicorn.conf
-./create_db.sh
+sudo ./create_db.sh
